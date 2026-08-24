b667e20690bcdb22f5369b68061b815ff4ecf139
Chuck McCallum <mccallucc@gmail.com>
2026-08-24 14:17:13 -0400

    logo

--- source/conf.py
+++ source/conf.py
@@ -15,7 +15,6 @@ release = '0.15.1' # Match the library version.
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
-html_favicon = "favicon.ico"
 templates_path = ['_templates']
 exclude_patterns = []
 
@@ -26,3 +25,5 @@ exclude_patterns = []
 
 html_theme = "pydata_sphinx_theme"
 html_static_path = ['_static']
+html_logo = "_static/images/opendp-logo.png"
+html_favicon = "favicon.ico"
